ac5b7de9d9d3a5bf6e06d54cfe53e56fac5f7721
LAB13_USER ON WS19 <L13X16W19@LabHCI.net>
2025-09-24 16:50:06 +0800

    feature(garbo_app): setup templates

--- garboproj1/garboproj1/settings.py
+++ garboproj1/garboproj1/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'garboproj1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/pos_app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
